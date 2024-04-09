5df866377f7e1f8e223ed8c00309ac2fcd04cd92
Matheus Marinho <matheus.marinhodsp@gmail.com>
2024-04-09 13:02:01 -0300

    feat: Aula02 - Implementa rota para coletar informações do usuario

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 import { createEvent } from "./routes/create-event";
 import { getEvent } from "./routes/get-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -16,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent);
 app.register(getEvent);
 app.register(registerForEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running on port 3333 🚀");
