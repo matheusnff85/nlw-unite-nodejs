87f9af0ba04d3809a3342e48d1de7342e637b365
Matheus Marinho <matheus.marinhodsp@gmail.com>
2024-04-09 15:19:59 -0300

    feat: Aula 03 finalizando aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
@@ -8,16 +12,41 @@ import { createEvent } from "./routes/create-event";
 import { getEvent } from "./routes/get-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
+
+import { errorHandler } from "./error-handler";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.register(fastifyCors, { origin: "*" });
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "Pass In",
+      description:
+        "Rotas da API para o backend da aplicação Pass In feita durante o NLW Unite da Rocketseat !!",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUI, { routePrefix: "/docs" });
+
 app.register(createEvent);
 app.register(getEvent);
 app.register(registerForEvent);
 app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+
+app.setErrorHandler(errorHandler);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running on port 3333 🚀");
